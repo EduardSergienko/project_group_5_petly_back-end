d3bc3a69dde22b8d7482cce18826da9442afd9ec
Dmytro-H13 <garnazhenkobb@gmail.com>
2022-11-09 21:20:25 +0100

    template changes

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+module.exports = app;
