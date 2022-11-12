780080c04830e9933efb2abc0142f71f88a1ca55
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-11-12 13:39:30 +0200

    add schema animal and helpers

---
+++ db/animal-model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+const Joi = require('joi');
+const handleSaveErrors = require("../helpers/handleSaveErrors");
+
+const regBirthDay = /(\d{2}).(\d{2}).(\d{4})/
+
+const animalSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+   },
+    dateOfBirth:{
+        type: String,
+        require: true,
+        formData: regBirthDay
+    },
+    breed:{
+        type: String,
+        require: true,
+    },
+    comments:{
+        type: String,
+    },
+     owner: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+
+},{ versionKey: false, timestamps: true }
+);
+
+animalSchema.post("save", handleSaveErrors);
+
+const Animal = model("animal", animalSchema);
+
+model.export = Animal;
