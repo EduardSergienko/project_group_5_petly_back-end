f7e42637fd402d4bc63edb61c1658fd6c04959a7
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-11-13 22:15:30 +0200

    conflict

--- routes/api/animals.js
+++ routes/api/animals.js
@@ -1,15 +1,13 @@
 const express = require("express");
-// const Animal = require("../../db/animal-model");
-// const ApiErrorsTemplate = require("../../helpers/errors");
+
 const ctrl = require("../../controllers/animalControllers");
 const { addAnimalJoiSchema } = require("../../db/animal-model");
 const { asyncWrapper } = require("../../helpers/api-helpers");
 const { authenticate } = require("../../middlewares/auth-middleware");
 const isValidId = require("../../middlewares/isValidId");
 const validateBody = require("../../middlewares/validateBody");
-const router = express.Router();
 
-// router.get("/");
+const router = express.Router();
 
 router.post(
   "/",
