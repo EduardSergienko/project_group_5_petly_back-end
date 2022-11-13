84ccc2c8e800e36aa8480c98e6f620a68e665478
Dmytro-H13 <garnazhenkobb@gmail.com>
2022-11-13 20:43:48 +0100

    wrapper

--- routes/api/news.js
+++ routes/api/news.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const getNewsController = require("../../controllers/news-controller");
+const { asyncWrapper } = require("../../helpers/api-helpers");
 
-router.get("/", getNewsController);
+router.get("/", asyncWrapper(getNewsController));
 
 module.exports = router;
