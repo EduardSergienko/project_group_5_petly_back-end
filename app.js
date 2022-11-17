ffdaaf68a2793888821ffcd3e6a9eb58366eba66
Dmytro-H13 <garnazhenkobb@gmail.com>
2022-11-17 21:40:28 +0100

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 const { errorHandler } = require("./helpers/api-helpers");
 const noticesRouter = require("./routes/api/notices.js");
@@ -16,7 +17,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static(`${__dirname} /public`));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/api/notices", noticesRouter);
 app.use("/api/auth", authRouter);
