274634e7e6ab9f0b0f1cb0fa831c3f36b2c4e850
Dmytro-H13 <garnazhenkobb@gmail.com>
2022-11-22 18:27:45 +0100

    change resize on photo

--- helpers/resize-avatar.js
+++ helpers/resize-avatar.js
@@ -4,7 +4,7 @@ const { ApiErrorsTemplate } = require("../helpers/errors");
 const resizeAvatar = async (path) => {
   try {
     const avatar = await Jimp.read(`${path}`);
-    avatar.resize(240, 240);
+    avatar.resize(350, 288);
     avatar.write(path);
   } catch (error) {
     throw new ApiErrorsTemplate(400, "Unsupported MIME type");
