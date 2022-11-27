69349e87245ca8d7953bd67dc292acd8d0cd164d
Dmytro-H13 <garnazhenkobb@gmail.com>
2022-11-27 18:52:41 +0100

    add sort for news

--- services/news-service.js
+++ services/news-service.js
@@ -2,7 +2,10 @@ const News = require("../db/news-model");
 
 const getNews = async (skip, limit) => {
   try {
-    const data = await News.find({}).skip(skip).limit(limit);
+    const data = await News.find({})
+      .sort({ date: "desc" })
+      .skip(skip)
+      .limit(limit);
     return data;
   } catch (error) {
     return error;
