cdf3e58c1f599db78bb06686adb1fc6b34323f54
EduardSergienko <katleraster@gmail.com>
2022-12-14 22:44:50 +0200

    test

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const mongoose = require("mongoose");
 const { MONGO_URL, PORT } = process.env;
 
 const start = async () => {
-  try {
-    await mongoose.connect(MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      ignoreUndefined: true,
-    });
-    console.log("Database connection successful");
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    });
-  } catch (error) {
-    console.log(error.message);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(MONGO_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			ignoreUndefined: true,
+		});
+		console.log("Database connection successful");
+		app.listen(PORT, () => {
+			console.log(`Server running. Use our API on port: ${PORT}`);
+		});
+	} catch (error) {
+		console.log(error.message);
+		process.exit(1);
+	}
 };
 
 start();
