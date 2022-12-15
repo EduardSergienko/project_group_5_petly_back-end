83339dc04fe86a6cc89a1ba869b4439c609f577e
EduardSergienko <katleraster@gmail.com>
2022-12-15 12:14:11 +0200

    Update cors-options.js

--- helpers/cors-options.js
+++ helpers/cors-options.js
@@ -1,18 +1,18 @@
 const allowList = [
-  "https://project-group-5-petly.netlify.app",
-  "http://localhost:3000",
+	"https://find-your-awesome-petly.netlify.app",
+	"http://localhost:3000",
 ];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200,
+	origin: (origin, callback) => {
+		if (allowList.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true,
+	optionsSuccessStatus: 200,
 };
 
 module.exports = corsOptions;
